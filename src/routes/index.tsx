93652611938728b3d69cc5c177ce0e9b1edf3193
Nikola Skadric <nikolaskadric.ns@gmail.com>
2026-06-17 00:23:53 +0200

    inital router setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-function App() {
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/')({
+	component: HomePage,
+});
+
+function HomePage() {
 	return (
 		<main className="flex min-h-full items-center justify-center">
 			<h1 className="text-2xl font-medium text-neutral-800">Nura Gallery</h1>
 		</main>
 	);
 }
-
-export default App;
